10353758dd2c8c6b5072464125b7cab046ba945d
pdinkins <parkerdinkins@gmail.com>
2021-05-20 17:43:09 -0400

    winbox site update

---
+++ new/js/main.js
@@ -0,0 +1,43 @@
+const about = document.querySelector('#about')
+const contact = document.querySelector('#contact')
+const aboutContent = document.querySelector('#about-content')
+const contactContent = document.querySelector('#contact-content')
+
+about.addEventListener('click', () => {
+  const aboutBox = new WinBox({
+    title: 'About Me',
+    // modal: true,
+    width: '400px',
+    height: '400px',
+    top: 15,
+    right: 15,
+    bottom: 15,
+    left: 15,
+    mount: aboutContent,
+    onfocus: function () {
+      this.setBackground('#00aa00')
+    },
+    onblur: function () {
+      this.setBackground('#777')
+    },
+  })
+})
+
+contact.addEventListener('click', () => {
+  const contactBox = new WinBox({
+    title: 'Contact Me',
+    width: '400px',
+    height: '400px',
+    top: 15,
+    right: 15,
+    bottom: 15,
+    left: 15,
+    mount: contactContent,
+    onfocus: function () {
+      this.setBackground('#00aa00')
+    },
+    onblur: function () {
+      this.setBackground('#777')
+    },
+  })
+})
